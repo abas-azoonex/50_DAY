cbd5abdc560b13b9e1306c152c017416e5e8afa6
abas-azoonex <abasyoutubsm@gmail.com>
2025-11-14 06:02:10 -0500

    Work the local-storage

--- day1/app/app.js
+++ day1/app/app.js
@@ -1,12 +1,18 @@
-var container = document.querySelectorAll(".panel");
-container.forEach(function (item) {
-    item.addEventListener('click', function (tiem) {
-        removeHandler();
-        item.classList.add('active');
-    });
-});
-var removeHandler = function () {
-    container.forEach(function (item) {
-        item.classList.remove('active');
-    });
-};
+var buttonSave = document.getElementById("save_local_storage");
+var buttonRemove = document.getElementById("remove_local_storage");
+var demo = document.getElementById("demo");
+console.log(buttonSave);
+function saveLocalStorage() {
+    localStorage.setItem("myCat", "Tom");
+}
+function remove_local_storage() {
+    localStorage.clear();
+}
+buttonSave === null || buttonSave === void 0 ? void 0 : buttonSave.addEventListener("click", saveLocalStorage);
+buttonRemove === null || buttonRemove === void 0 ? void 0 : buttonRemove.addEventListener("click", remove_local_storage);
+(function checkDom() {
+    var getMyCat = localStorage.getItem("myCat");
+    if (getMyCat) {
+        demo === null || demo === void 0 ? void 0 : demo.innerHTML = "<>".concat(getMyCat, "<>");
+    }
+})();
--- day1/app/app.ts
+++ day1/app/app.ts
@@ -1,15 +1,28 @@
-let container = document.querySelectorAll(".panel")!
 
-container.forEach((item) => {
-    item.addEventListener('click',tiem => {
-        removeHandler()
-        item.classList.add('active')
-    })
-});
+const buttonSave = document.getElementById("save_local_storage")
+const buttonRemove = document.getElementById("remove_local_storage");
 
 
-const removeHandler = ()=>{
-    container.forEach(item=>{
-        item.classList.remove('active')
-    })
+const demo = document.getElementById("demo");
+console.log(buttonSave)
+
+function saveLocalStorage (){
+    localStorage.setItem("myCat","Tom")
 }
+
+function remove_local_storage(){
+    localStorage.clear()
+}
+
+
+buttonSave?.addEventListener("click",saveLocalStorage)
+buttonRemove?.addEventListener("click",remove_local_storage);
+
+
+(function checkDom(){
+    const getMyCat = localStorage.getItem("myCat");
+    if(getMyCat){
+    demo?.innerHTML = `<>${getMyCat}<>`
+    }
+    
+})()
